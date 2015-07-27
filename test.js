c12224b5899c1ba934bab4f68f5c757379893582
clickphp <evgen.veprickiy@gmail.com>
2015-07-27 13:35:42 +0300

    add new strings with var = try learn variables

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
 /**
  * Created by click on 25.07.15.
  */
+
 //this command will show alert window with test "No error"
-alert("No error");
+//"use strict";
+//alert("No error");
 //this command will show two other windows what will shows from firs to last step by step
-[1, 2]. forEach(alert);
+//[1, 2]. forEach(alert);
+
+    // 'var' its is variable. so i just set var name; then save in vat string
+var say = 'Hello', name = 'Click', age = '32 y.o.'; // i can use meny variables in var like in this exercise
+    alert (say);
+    alert (name);
+    alert (age);
+
