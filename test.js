b95d32d0e2b0eeb7221ddf349a94f44f1f24012f
clickphp <evgen.veprickiy@gmail.com>
2015-08-10 21:14:18 +0300

    chore new operators

--- test.js
+++ test.js
@@ -8,7 +8,22 @@
 //[1, 2]. forEach(alert);
 
     // 'var' its is variable. so i just set var name; then save in vat string
+
+//var planet = "Earth", username = "Click";
+//alert (planet);
+//alert (username);
 "use strict";
-var planet = "Earth", username = "Click";
-alert (planet);
-alert (username);
+var count = 2+3;
+    alert (count);
+
+var x=1;
+x = -x;
+alert(x);
+
+var apples = 2;
+var pinapples = 8;
+alert(+apples * +pinapples);
+
+var digit=5;
+digit --;
+alert(digit);
