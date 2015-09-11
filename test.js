79d96d6f29e9df22b9f6b12881c9629f9e5f1422
clickphp <evgen.veprickiy@gmail.com>
2015-09-11 11:26:19 +0300

    test change if to switch

--- test.js
+++ test.js
@@ -92,13 +92,20 @@ function pushit4 () {
 
 function pushit5 () {
 
-    var browser = prompt('type here name of browser', '');
-        if (browser == 'IE') alert( 'О, да у вас IE!' );
-            else if (browser == 'Chrome' || browser == 'Firefox' || browser == 'Safari' || browser == 'Opera')
-            alert ('Да, и эти браузеры мы поддерживаем');
-
-         else alert('Мы надеемся, что и в вашем браузере все ок!' );
-
+    var a = +prompt('a?', '');
+    switch (a){
+        case 0:
+            alert('0');
+            break;
+        case 1:
+            alert('1');
+            break;
+        case 2:
+        case 3:
+            alert('2,3');
+            break;
+        default: alert("Nop, i dont know this digit");
+    }
 }
 
 //document.onkeydown = function(evt) {
